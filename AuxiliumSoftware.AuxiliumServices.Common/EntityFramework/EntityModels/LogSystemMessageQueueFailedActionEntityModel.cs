ef394704ed0d92c70d70804b2823b986abeda3a8
Cerys <cerysamberlewis@gmail.com>
2026-05-29 17:46:01 +0100

    new logging tables for the task runner service

---
+++ AuxiliumSoftware.AuxiliumServices.Common/EntityFramework/EntityModels/LogSystemMessageQueueFailedActionEntityModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace AuxiliumSoftware.AuxiliumServices.Common.EntityFramework.EntityModels
+{
+    public class LogSystemMessageQueueFailedActionEntityModel
+    {
+        /// <summary>
+        /// </summary>
+        [Key]
+        public required Guid Id { get; set; }
+        /// <summary>
+        /// </summary>
+        public required DateTime CreatedAt { get; set; }
+
+
+
+
+
+        /// <summary>
+        /// </summary>
+        public required Guid MessageId { get; set; }
+        /// <summary>
+        /// </summary>
+        public required DateTime MessageCreatedAt { get; set; }
+        /// <summary>
+        /// </summary>
+        public required string MessageCorrelationId { get; set; }
+        /// <summary>
+        /// </summary>
+        public required string MessageRoutingKey { get; set; }
+        /// <summary>
+        /// </summary>
+        public required string MessageJson { get; set; }
+        /// <summary>
+        /// </summary>
+        public required string ExceptionType { get; set; }
+        /// <summary>
+        /// </summary>
+        public required string ExceptionMessage { get; set; }
+        /// <summary>
+        /// </summary>
+        public required string ExceptionStackTrace { get; set; }
+    }
+}
---
+++ AuxiliumSoftware.AuxiliumServices.Common/EntityFramework/EntityModels/LogSystemMessageQueueSentEmailEntityModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace AuxiliumSoftware.AuxiliumServices.Common.EntityFramework.EntityModels
+{
+    public class LogSystemMessageQueueSentEmailEntityModel
+    {
+        /// <summary>
+        /// </summary>
+        [Key]
+        public required Guid Id { get; set; }
+        /// <summary>
+        /// </summary>
+        public required DateTime CreatedAt { get; set; }
+
+
+
+
+
+        /// <summary>
+        /// </summary>
+        public required Guid MessageId { get; set; }
+        /// <summary>
+        /// </summary>
+        public required DateTime MessageCreatedAt { get; set; }
+        /// <summary>
+        /// </summary>
+        public required string MessageCorrelationId { get; set; }
+        /// <summary>
+        /// </summary>
+        public required string MessageRoutingKey { get; set; }
+        /// <summary>
+        /// </summary>
+        public required string MessageJson { get; set; }
+
+
+
+
+
+        /// <summary>
+        /// </summary>
+        public required string EmailRecipientAddress { get; set; }
+        /// <summary>
+        /// </summary>
+        public required string EmailRecipientName { get; set; }
+        /// <summary>
+        /// </summary>
+        public required string EmailLanguage { get; set; }
+        /// <summary>
+        /// </summary>
+        public required string EmailTemplate { get; set; }
+        /// <summary>
+        /// </summary>
+        public required string EmailSubject { get; set; }
+        /// <summary>
+        /// </summary>
+        public required string EmailBodyHtml { get; set; }
+        /// <summary>
+        /// </summary>
+        public required string EmailBodyTxt { get; set; }
+    }
+}
