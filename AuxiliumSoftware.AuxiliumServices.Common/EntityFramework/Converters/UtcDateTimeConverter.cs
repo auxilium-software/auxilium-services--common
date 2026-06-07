26ec1369ccaa02293a71974501b94325bb70e231
Cerys <cerysamberlewis@gmail.com>
2026-06-07 19:38:47 +0100

    Create UtcDateTimeConverter.cs

---
+++ AuxiliumSoftware.AuxiliumServices.Common/EntityFramework/Converters/UtcDateTimeConverter.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuxiliumSoftware.AuxiliumServices.Common.EntityFramework.Converters
+{
+    public class UtcDateTimeConverter : ValueConverter<DateTime, DateTime>
+    {
+        public UtcDateTimeConverter() : base(
+            v => v,                                         // writing: already UTC
+            v => DateTime.SpecifyKind(v, DateTimeKind.Utc)) // reading: tag as UTC
+        { }
+    }
+}
