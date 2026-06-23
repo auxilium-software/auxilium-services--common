dbdfb1a150ae6c54d04bede53ab4b7a8d79fb9d6
Cerys <cerysamberlewis@gmail.com>
2026-06-23 16:33:40 +0100

    Update RabbitMQQueuesConfigurationSection.cs

--- AuxiliumSoftware.AuxiliumServices.Common/Configuration/Sections/Databases/RabbitMQ/RabbitMQQueuesConfigurationSection.cs
+++ AuxiliumSoftware.AuxiliumServices.Common/Configuration/Sections/Databases/RabbitMQ/RabbitMQQueuesConfigurationSection.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace AuxiliumSoftware.AuxiliumServices.Common.Configuration.Sections.Databases
+namespace AuxiliumSoftware.AuxiliumServices.Common.Configuration.Sections.Databases.RabbitMQ
 {
     public class RabbitMQQueuesConfigurationSection
     {
