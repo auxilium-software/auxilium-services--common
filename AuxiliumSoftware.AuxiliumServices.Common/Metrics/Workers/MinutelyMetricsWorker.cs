9e22e9b6aeb21c4316ec66b6f39b305529ff071a
Cerys <cerysamberlewis@gmail.com>
2026-06-23 15:29:12 +0100

    workers

---
+++ AuxiliumSoftware.AuxiliumServices.Common/Metrics/Workers/MinutelyMetricsWorker.cs
@@ -0,0 +1,18 @@
+﻿using AuxiliumSoftware.AuxiliumServices.Common.Enumerators;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuxiliumSoftware.AuxiliumServices.Common.Metrics.Workers
+{
+    public sealed class MinutelyMetricsWorker : MetricCollectionWorker
+    {
+        private const string Schedule = "* * * * *"; // every minute
+        protected override MetricCadence Cadence => MetricCadence.Minutely;
+
+        public MinutelyMetricsWorker(IServiceScopeFactory scopeFactory, ILogger<MinutelyMetricsWorker> logger)
+            : base(Schedule, scopeFactory, logger) { }
+    }
+}
