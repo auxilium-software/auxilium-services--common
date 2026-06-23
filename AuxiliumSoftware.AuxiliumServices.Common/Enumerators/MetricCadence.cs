dd973b804a031667cf17121ddb076b97af23a4f4
Cerys <cerysamberlewis@gmail.com>
2026-06-23 15:23:26 +0100

    Create MetricCadence.cs

---
+++ AuxiliumSoftware.AuxiliumServices.Common/Enumerators/MetricCadence.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuxiliumSoftware.AuxiliumServices.Common.Enumerators
+{
+    public enum MetricCadence
+    {
+        Hourly,
+        Minutely,
+    }
+}
