93fc315ca9901c34071c61364fab0db30e458137
Cerys <cerysamberlewis@gmail.com>
2026-06-23 15:26:56 +0100

    records

---
+++ AuxiliumSoftware.AuxiliumServices.Common/Metrics/Common/MetricSample.cs
@@ -0,0 +1,12 @@
+﻿using AuxiliumSoftware.AuxiliumServices.Common.EntityFramework.Enumerators;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuxiliumSoftware.AuxiliumServices.Common.Metrics.Common
+{
+    public readonly record struct MetricSample(
+        SystemMetricKeyEnum Key,
+        double Value
+    );
+}
---
+++ AuxiliumSoftware.AuxiliumServices.Common/Metrics/Common/ProcessMetricKeys.cs
@@ -0,0 +1,13 @@
+﻿using AuxiliumSoftware.AuxiliumServices.Common.EntityFramework.Enumerators;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuxiliumSoftware.AuxiliumServices.Common.Metrics.Common
+{
+    public sealed record ProcessMetricKeys(
+        SystemMetricKeyEnum Cpu,
+        SystemMetricKeyEnum Memory,
+        SystemMetricKeyEnum Uptime
+    );
+}
