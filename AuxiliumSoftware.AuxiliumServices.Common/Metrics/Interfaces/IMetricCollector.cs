1433b2c034cd15d7a259100d619df8501b7c9ca5
Cerys <cerysamberlewis@gmail.com>
2026-06-23 15:29:33 +0100

    Create IMetricCollector.cs

---
+++ AuxiliumSoftware.AuxiliumServices.Common/Metrics/Interfaces/IMetricCollector.cs
@@ -0,0 +1,15 @@
+﻿using AuxiliumSoftware.AuxiliumServices.Common.Enumerators;
+using AuxiliumSoftware.AuxiliumServices.Common.Metrics.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuxiliumSoftware.AuxiliumServices.Common.Metrics.Interfaces
+{
+    public interface IMetricCollector
+    {
+        MetricCadence Cadence => MetricCadence.Hourly;
+
+        Task<IReadOnlyList<MetricSample>> CollectAsync(CancellationToken ct);
+    }
+}
