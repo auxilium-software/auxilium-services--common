a2b3ecd887dd0bcfe2fe4a07d19aa79f39b59808
Cerys <cerysamberlewis@gmail.com>
2026-06-24 02:08:00 +0100

    3.0.45.0

---
+++ AuxiliumSoftware.AuxiliumServices.Common/EntityFramework/Enumerators/CaseTimelineEntryTypeEnum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace AuxiliumSoftware.AuxiliumServices.Common.EntityFramework.Enumerators
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum CaseTimelineEntryTypeEnum
+    {
+        [JsonPropertyName("note.user")]
+        Note_User,
+
+        [JsonPropertyName("note.system")]
+        Note_System,
+    }
+}
