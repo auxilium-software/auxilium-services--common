885d219741237a3f5fdb0dbc754f57e54c580cc8
Cerys <cerysamberlewis@gmail.com>
2026-06-29 15:51:57 +0100

    ->

--- AuxiliumSoftware.AuxiliumServices.Common/Metrics/Enumerators/SystemMetricLabelEnum.cs
+++ AuxiliumSoftware.AuxiliumServices.Common/Metrics/Enumerators/SystemMetricLabelEnum.cs
@@ -3,16 +3,18 @@ using System.Collections.Generic;
 using System.Text;
 using System.Text.Json.Serialization;
 
-namespace AuxiliumSoftware.AuxiliumServices.Common.EntityFramework.Enumerators
+namespace AuxiliumSoftware.AuxiliumServices.Common.Metrics.Enumerators
 {
     [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum SystemMetricLabelEnum
     {
+        // rabbitmq queues
         [JsonPropertyName("rabbitmq.queue.notifications")]
         RabbitMq_Notifications,
         [JsonPropertyName("rabbitmq.queue.dead-letter")]
         RabbitMq_DeadLetter,
 
+        // storage roots
         [JsonPropertyName("storage.form-data")]
         Storage_FormData,
         [JsonPropertyName("storage.aux-lfs")]
