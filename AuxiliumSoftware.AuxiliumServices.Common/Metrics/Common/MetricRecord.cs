7eff7462dc13ba82b95fce11e5a8b4c969c5a0ce
Cerys <cerysamberlewis@gmail.com>
2026-06-29 16:04:13 +0100

    Create MetricRecord.cs

---
+++ AuxiliumSoftware.AuxiliumServices.Common/Metrics/Common/MetricRecord.cs
@@ -0,0 +1,11 @@
+﻿using AuxiliumSoftware.AuxiliumServices.Common.Metrics.Enumerators;
+
+namespace AuxiliumSoftware.AuxiliumServices.Common.Metrics.Common
+{
+    public readonly record struct MetricRecord(
+        SystemMetricKeyEnum Key,
+        DateTime TimestampUtc,
+        double Value,
+        SystemMetricLabelEnum? Label = null
+    );
+}
