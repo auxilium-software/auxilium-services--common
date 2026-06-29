e11c819dc935647de91a1993edf85c4bdee685c8
Cerys <cerysamberlewis@gmail.com>
2026-06-29 16:04:10 +0100

    Create DiskRow.cs

---
+++ AuxiliumSoftware.AuxiliumServices.Common/Metrics/Common/DiskRow.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace AuxiliumSoftware.AuxiliumServices.Common.Metrics.Common
+{
+    internal sealed record DiskRow
+    {
+        [JsonPropertyName("t")] public DateTime T { get; init; }
+        [JsonPropertyName("v")] public double V { get; init; }
+        [JsonPropertyName("l")] public string? L { get; init; }
+    }
+}
