c693816b2695a31087726543b75f6a48f7f4a20d
Cerys <cerysamberlewis@gmail.com>
2026-06-29 15:51:30 +0100

    added config values

--- AuxiliumSoftware.AuxiliumServices.Common/Configuration/Sections/FileSystem/RootStorageDirectoriesConfigurationSection.cs
+++ AuxiliumSoftware.AuxiliumServices.Common/Configuration/Sections/FileSystem/RootStorageDirectoriesConfigurationSection.cs
@@ -6,13 +6,17 @@ namespace AuxiliumSoftware.AuxiliumServices.Common.Configuration.Sections.FileSy
 {
     public class RootStorageDirectoriesConfigurationSection
     {
+        public string FormData { get; set; } = null!;
         public string AuxLFS { get; set; } = null!;
-        
+        public string Metrics { get; set; } = null!;
+
 
 
         public void Validate()
         {
-            if (string.IsNullOrWhiteSpace(AuxLFS))  throw new InvalidOperationException("Configuration value 'RootStorageDirectories->AuxLFS' is missing.");
+            if (string.IsNullOrWhiteSpace(FormData))    throw new InvalidOperationException("Configuration value 'RootStorageDirectories->FormData' is missing.");
+            if (string.IsNullOrWhiteSpace(AuxLFS))      throw new InvalidOperationException("Configuration value 'RootStorageDirectories->AuxLFS' is missing.");
+            if (string.IsNullOrWhiteSpace(Metrics))     throw new InvalidOperationException("Configuration value 'RootStorageDirectories->Metrics' is missing.");
         }
     }
 }
