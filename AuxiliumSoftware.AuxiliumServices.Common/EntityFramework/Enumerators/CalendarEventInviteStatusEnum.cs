c0bb6412d755c7f3dfa6e06949a31bcffc49e873
Cerys <cerysamberlewis@gmail.com>
2026-08-05 14:06:52 +0100

    calendar stuff

---
+++ AuxiliumSoftware.AuxiliumServices.Common/EntityFramework/Enumerators/CalendarEventInviteStatusEnum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace AuxiliumSoftware.AuxiliumServices.Common.EntityFramework.Enumerators
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum CalendarEventInviteStatusEnum
+    {
+        [JsonPropertyName("pending")]
+        Pending,
+
+        [JsonPropertyName("accepted")]
+        Accepted,
+
+        [JsonPropertyName("declined")]
+        Declined,
+
+        [JsonPropertyName("tenative")]
+        Tentative,
+    }
+}
---
+++ AuxiliumSoftware.AuxiliumServices.Common/EntityFramework/Enumerators/DataEnumeratorScopeEnum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace AuxiliumSoftware.AuxiliumServices.Common.EntityFramework.Enumerators
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum DataEnumeratorScopeEnum
+    {
+        [JsonPropertyName("additional_property")]
+        AdditionalProperty,
+
+        [JsonPropertyName("calendar_event_category")]
+        CalendarEventCategory,
+    }
+}
