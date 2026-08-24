4db352d81cb1ef37cc3aa9eaa9f3c33ea633bc18
Cerys <cerysamberlewis@gmail.com>
2026-08-24 18:07:11 +0100

    Create ITenantScopedEntityModel.cs

---
+++ AuxiliumSoftware.AuxiliumServices.Common/EntityFramework/ITenantScopedEntityModel.cs
@@ -0,0 +1,13 @@
+﻿using AuxiliumSoftware.AuxiliumServices.Common.EntityFramework.EntityModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuxiliumSoftware.AuxiliumServices.Common.EntityFramework
+{
+    public interface ITenantScopedEntityModel
+    {
+        Guid TenantId { get; set; }
+        TenantEntityModel? Tenant { get; set; }
+    }
+}
