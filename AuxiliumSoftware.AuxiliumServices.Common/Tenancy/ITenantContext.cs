05e79b16a07514ccefb4dc69eca628f44f797ea2
Cerys <cerysamberlewis@gmail.com>
2026-08-24 18:07:20 +0100

    Create ITenantContext.cs

---
+++ AuxiliumSoftware.AuxiliumServices.Common/Tenancy/ITenantContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuxiliumSoftware.AuxiliumServices.Common.Tenancy
+{
+    public interface ITenantContext
+    {
+        Guid TenantId { get; }
+        bool IsResolved { get; }
+    }
+}
