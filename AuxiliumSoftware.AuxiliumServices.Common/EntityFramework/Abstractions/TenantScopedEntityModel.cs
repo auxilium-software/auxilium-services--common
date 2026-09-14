8065901392187193a9e41db558e76485654b466b
Cerys <cerysamberlewis@gmail.com>
2026-09-14 17:10:40 +0100

    `3.0.53.4`

--- AuxiliumSoftware.AuxiliumServices.Common/EntityFramework/Abstractions/TenantScopedEntityModel.cs
+++ AuxiliumSoftware.AuxiliumServices.Common/EntityFramework/Abstractions/TenantScopedEntityModel.cs
@@ -8,7 +8,7 @@ namespace AuxiliumSoftware.AuxiliumServices.Common.EntityFramework.Abstractions
     public abstract class TenantScopedEntityModel
     {
         public required Guid Id { get; set; }
-        public Guid TenantId { get; set; }
+        public Guid TenantId { get; set; } // <-- not required as it's handled by the interceptor
 
 
 
