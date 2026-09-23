b01c6677d5d5a3ca869731f470d6d4e6e6da3874
Cerys <cerysamberlewis@gmail.com>
2026-09-23 23:15:57 +0100

    Create LogMergeEventEntityModelBase.cs

---
+++ AuxiliumSoftware.AuxiliumServices.Common/EntityFramework/Abstractions/LogMergeEventEntityModelBase.cs
@@ -0,0 +1,28 @@
+﻿using AuxiliumSoftware.AuxiliumServices.Common.EntityFramework.EntityModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuxiliumSoftware.AuxiliumServices.Common.EntityFramework.Abstractions
+{
+    public abstract class LogMergeEventEntityModelBase : TenantScopedEntityModelBase
+    {
+        public required Guid CreatedByUserId { get; set; }
+
+
+
+
+
+        public required string FieldResolutionsJson { get; set; }
+
+        public required string SurvivorPreviousValuesJson { get; set; }
+
+        public required string MergedSnapshotJson { get; set; }
+
+
+
+
+
+        public UserEntityModel? CreatedByUser { get; set; }
+    }
+}
