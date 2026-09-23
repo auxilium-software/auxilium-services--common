7fb5db28f6aaea976a990f7d6f2a046f1e58dc99
Cerys <cerysamberlewis@gmail.com>
2026-09-23 18:49:57 +0100

    data merge attributes

---
+++ AuxiliumSoftware.AuxiliumServices.Common/Attributes/DataMergeBehaviourAttribute.cs
@@ -0,0 +1,18 @@
+﻿using AuxiliumSoftware.AuxiliumServices.Common.Enumerators;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuxiliumSoftware.AuxiliumServices.Common.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class DataMergeBehaviourAttribute : Attribute
+    {
+        public DataMergeBehaviourEnum Behaviour { get; }
+
+        public DataMergeBehaviourAttribute(DataMergeBehaviourEnum behaviour)
+        {
+            Behaviour = behaviour;
+        }
+    }
+}
