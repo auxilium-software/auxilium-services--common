f4b2f9c0c3cfa35fd7379c47c70d1ef714cff438
Cerys <cerysamberlewis@gmail.com>
2026-09-23 23:13:09 +0100

    Create LogCaseMergeEventEntityModel.cs

---
+++ AuxiliumSoftware.AuxiliumServices.Common/EntityFramework/EntityModels/LogCaseMergeEventEntityModel.cs
@@ -0,0 +1,27 @@
+﻿using AuxiliumSoftware.AuxiliumServices.Common.EntityFramework.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuxiliumSoftware.AuxiliumServices.Common.EntityFramework.EntityModels
+{
+    public class LogCaseMergeEventEntityModel : LogMergeEventEntityModelBase
+    {
+        public required Guid SurvivorCaseId { get; set; }
+
+        public required Guid TombstoneCaseId { get; set; }
+
+
+
+
+
+        public CaseEntityModel? SurvivorCase { get; set; }
+        public CaseEntityModel? TombstoneCase { get; set; }
+
+
+
+
+
+        public ICollection<LogCaseMergeEventRowChangeEntityModel>? RowChanges { get; set; }
+    }
+}
