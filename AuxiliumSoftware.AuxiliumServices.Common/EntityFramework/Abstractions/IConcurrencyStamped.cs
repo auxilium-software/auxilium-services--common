429fa318ef877c918b6dace07ed9e17d0aee614e
Cerys <cerysamberlewis@gmail.com>
2026-09-24 12:59:07 +0100

    Create IConcurrencyStamped.cs

---
+++ AuxiliumSoftware.AuxiliumServices.Common/EntityFramework/Abstractions/IConcurrencyStamped.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuxiliumSoftware.AuxiliumServices.Common.EntityFramework.Abstractions
+{
+    public interface IConcurrencyStamped
+    {
+        Guid ConcurrencyStamp { get; set; }
+    }
+}
